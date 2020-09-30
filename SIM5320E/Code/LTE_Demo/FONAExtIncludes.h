fb90a1ae5a1d64021067458d7b9f30628a5a6198
phamvanphuckks <vietnam9094@gmail.com>
2020-09-30 17:08:43 +0700

    test http using AT command done

---
+++ SIM5320E/Code/LTE_Demo/FONAExtIncludes.h
@@ -0,0 +1,17 @@
+/*
+ * FONAExtIncludes.h -- system-wide includes
+ * This is part of the library for the Adafruit FONA Cellular Module
+ *
+ */
+
+
+#ifndef ADAFRUIT_FONA_LIBRARY_SRC_INCLUDES_FONAEXTINCLUDES_H_
+#define ADAFRUIT_FONA_LIBRARY_SRC_INCLUDES_FONAEXTINCLUDES_H_
+
+
+#include "FONAConfig.h"
+// include any system-wide includes required here
+
+
+
+#endif /* ADAFRUIT_FONA_LIBRARY_SRC_INCLUDES_FONAEXTINCLUDES_H_ */
